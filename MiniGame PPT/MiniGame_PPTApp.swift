e98621a0f5d589c04dfa356adfe8b3d89f739c03
FelipepPrado <felipepdelimaa@gmail.com>
2026-08-07 10:50:08 -0300

    Initial Commit

---
+++ MiniGame PPT/MiniGame_PPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiniGame_PPTApp.swift
+//  MiniGame PPT
+//
+//  Created by Felipe Prado de Lima on 07/08/26.
+//
+
+import SwiftUI
+
+@main
+struct MiniGame_PPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
